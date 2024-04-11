829eb5e3e81df5bbe8c52e37177436773f1ca73e
NotOfficialAash <notofficial.aashrith@gmail.com>
2024-04-11 13:52:40 +0530

    Improved Design

--- Launcher/Launcher.cpp
+++ Launcher/Launcher.cpp
@@ -1,33 +1,32 @@
 #include <iostream>
 #include <windows.h>
-#include <string.h>
+#include <thread>
 #pragma comment(lib, "Winmm.lib")
 
 using namespace std;
 
-void countdown() {
-	int max_time = 0;
-	string status;
+void countdown(int maxTime) {
+	char dismiss;
+	for (int i = maxTime * 60 ; i > 0; i--) {
+		this_thread::sleep_for(1000ms);
+	}
 
-	cout << "Enter session limit in minutes: ";
-	cin >> max_time;
+	mciSendString(L"open time_up_sound.mp3 type mpegvideo alias timeUpSound", NULL, 0, NULL);
+	mciSendString(L"play timeUpSound from 0 to 1500 wait repeat", NULL, 0, NULL);
 
-	max_time = (max_time * 60) * 1000;
-	Sleep(max_time);
-	main();
+	while (dismiss != 'D') {
+		cout << "\nYour session time has expires. Take a break. Type D to dismiss and close.\n";
+		cin >> dismiss;
+	}
 
-	mciSendString(L"open \"D:/Programming/C++/Launcher/assets/time_up_sound.mp3\" type mpegvideo alias mp3", NULL, 0, NULL);
-	mciSendString(L"play mp3 from 0 to 1500 wait repeat", NULL, 0, NULL);
-
-	cout << "Session time has expired. Please take a break!\n Type dismiss to exit.";
-	cin >> status;
-	mciSendString(L"close mp3", NULL, 0, NULL);
+	mciSendString(L"stop timeUpSound", NULL, 0, NULL);
+	mciSendString(L"close timeUpSound", NULL, 0, NULL);
 }
 
-int main(){
-	string username = "NotOfficialAash";
-	cout << "Welcome " << username << endl;
-	countdown();
 
+int main(){
+	int sessionDuration;
+	cout << "Enter SESSION TIME in MINUTES";
+	cin >> sessionDuration;
 
 }
